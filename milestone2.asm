daeed870417d7d9fb91519a4415267701ae19695
Yosi Hatekar <yosi51010@gmail.com>
2020-12-10 15:09:00 -0500

    Added keyboard input

--- milestone2.asm
+++ milestone2.asm
@@ -13,104 +13,67 @@
 # - Base Address for Display: 0x10008000 ($gp)
 #
 # Which milestone is reached in this submission?
-# - Milestone 2?
+# - Milestone 2
 #####################################################################
-##### MACROS START HERE #####
-.macro pushStack
-addi $sp, $sp, -4
-sw $ra, 0($sp)
-.end_macro
-
-.macro popStack
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-.end_macro
-
-.macro sleep (%time)
-li $v0, 32
-li $a0, %time
-syscall
-.end_macro	
-####################################################################
 .data
 	displayAddress: .word 0x10008000
 	#Colours:
 	sky: .word 0x00FFFFFF
 	doodleBot: .word 0x00FFDD3C
 	platform: .word 0x0092DFF3
-	#Inputs:
-	left: .word 0x6A
-	right: .word 0x6B
-	start: .word 0x73
-	#Positions:
-	doodleX: .space 4
-	doodleY: .space 4
-	platformPosition: .space 4
 .text
 main:
 #### PROGRAM RUNS HERE ####
-checkInput: #checks for any keyboard input and acts accordingly.
-lw $t8, 0xffff0000 #checks for any input
-beq $t8, 1, checkKey #if there is input, moves to check which key
-j checkInput
-checkKey:
-lw $t2, 0xffff0004
-lw $t3, start
-lw $t4, left
-lw $t5, right 
-beq $t2, $t3, startScreen
-beq $t2, $t4, moveLeft
-beq $t2, $t5, moveRight
-jal jump
-j checkInput
-
-startScreen:
 jal drawBackground
+
+#draw THREE platforms RANDOMLY where 0<=x<=24 and 0<=y<=31
 jal generateRandom
 jal drawPlatform
-addi $t0, $zero, 2
-addi $t1, $zero, 28
-sw $t0, doodleX
-sw $t1, doodleY
-jal jump
-j checkInput
 
-moveLeft:
-jal moveLeftFunc
-jal jump
-j checkInput
+jal generateRandom
+jal drawPlatform
 
-moveRight:
-jal moveRightFunc
-jal jump
-j checkInput
+jal generateRandom
+jal drawPlatform
+
+
+#draw doodleBot where 1<=x<=29 and 0<=y<=28
+addi $a0, $zero, 2 #x=2
+addi $a1, $zero, 3 #y=3
+jal drawDoodle
+
+jump: #animate doodleBot to move across across screen
+addi $t5, $zero, 29 # i=29. This is starting at the bottom of the screen.
+addi $t6, $zero, 0# iterate upto j. This is the top of the screen.
+animateLoop:
+	beq $t5, $t6, animateExit #stop loop when $t5==$t6/ while i!=j
+	addi $a0, $t5, 0 #x=i
+	addi $a1,$t5, -1 #y=3
+	jal drawBackground
+	jal drawDoodle
+	addi $t5, $t5, -1 #/i-=1
+	li $v0, 32
+	li $a0, 100
+	syscall
+	j animateLoop #loops back to branch statement
+animateExit:
+
 
 li $v0, 10 #terminate the program gracefully
 syscall
+##### MACROS START HERE #####
+.macro pushStack
+addi $sp, $sp, -4
+
+sw $ra, 0($sp)
+.end_macro
+
+.macro popStack
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+.end_macro	
+
 ##### FUNCTIONS START HERE #####
-moveLeftFunc:
-lw $t0, doodleX
-subi $t0, $t0, 1
-sw $t0, doodleX
-jr $ra
-
-moveRightFunc:
-lw $t0, doodleX
-addi $t0, $t0, 1
-sw $t0, doodleX
-jr $ra
-
-jump:
-pushStack
-lw $a0, doodleX
-lw $a1, doodleY
-subi $a1, $a1, 1
-jal drawBackground
-jal drawPlatform
-jal drawDoodle
-sleep (100)
-popStack
-jr $ra
 
 drawBackground: #colours the whole screen with colour sky. No arguments. No returns.
 lw $t0, displayAddress #$t0 stores the base address for display
@@ -131,8 +94,7 @@ jr $ra #goes back to the location of function call
 
 drawPlatform: #draws a single platform 8 units long. 1 argument of starting coordinate in $a0. No returns.
 lw $t0, displayAddress # $t0 stores the base address for display
-lw $t9, platformPosition
-add $t0, $t0, $t9 #change the value of memory to new co-ordinate (instead of base), stored in $a0
+add $t0, $t0, $a0 #change the value of memory to new co-ordinate (instead of base), stored in $a0
 lw $t1, platform #stores the platform colour code in $t1
 add $t2, $zero, $zero #store 0 in $t2/i=0
 addi $t3, $zero, 8 #store 10 in #$t3, which is the platform length
@@ -160,46 +122,14 @@ li $a1, 32 #store max number such that y < 32
 syscall
 sll $t1, $a0, 7 #store the random number = 128y
 add $a0, $t0, $t1 #store 4x+128y in $a0 as argument for drawPlatform
-sw $a0, platformPosition
 jr $ra #goes back to the location of function call
 
 
 drawDoodle:#draw doodleBot at the specified position = (4*x)+(128*y), where 1<=x<=29 and 0<=y<=28. x,y stored in $a0, $a1. No returns
 lw $t0, displayAddress #$t0 stores the base address for display
 lw $t1, doodleBot #stores the doodleBot colour code in $t1
-lw $t5, doodleX #retrieves x-value
-lw $t6, doodleY #retrieves y-value
-sll $t2, $t5, 2 #stores 4*x in $t2
-sll $t3, $t6, 7 #stores 128*y in $t3
-add $t2, $t2, $t3 #stores (4*x)+(128*y) = position offset in $t2
-add $t0, $t0, $t2 #change the value of memory to new position (instead of base) calculated in $t2
-#head
-sw $t1, ($t0)
-sw $t1, 4($t0)
-#torso
-sw $t1, 124($t0)
-sw $t1, 128($t0)
-sw $t1, 132($t0)
-sw $t1, 136($t0)
-#abdomen
-sw $t1, 252($t0)
-sw $t1, 256($t0)
-sw $t1, 260($t0)
-sw $t1, 264($t0)
-#legs
-sw $t1, 380($t0)
-sw $t1, 392($t0)
-
-jr $ra #goes back to the location of function call
-
-
-clearDoodle:#draw doodleBot at the specified position = (4*x)+(128*y), where 1<=x<=29 and 0<=y<=28. x,y stored in $a0, $a1. No returns
-lw $t0, displayAddress #$t0 stores the base address for display
-lw $t1, sky #stores the sky colour code in $t1
-lw $t5, doodleX #retrieves x-value
-lw $t6, doodleY #retrieves y-value
-sll $t2, $t5, 2 #stores 4*x in $t2
-sll $t3, $t6, 7 #stores 128*y in $t3
+sll $t2, $a0, 2 #stores 4*x in $t2
+sll $t3, $a1, 7 #stores 128*y in $t3
 add $t2, $t2, $t3 #stores (4*x)+(128*y) = position offset in $t2
 add $t0, $t0, $t2 #change the value of memory to new position (instead of base) calculated in $t2
 #head
@@ -219,6 +149,9 @@ sw $t1, 264($t0)
 sw $t1, 380($t0)
 sw $t1, 392($t0)
 jr $ra #goes back to the location of function call
+
+
+
 
 
 
